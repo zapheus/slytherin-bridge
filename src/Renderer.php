b292e80732f6b1aaf26eb8c3dd8bf293eae598b5
Rougin Royce Gutib <rougingutib@gmail.com>
2018-02-23 11:25:57 +0800

    Add Renderer

---
+++ src/Renderer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Zapheus\Bridge\Slytherin;
+
+use Rougin\Slytherin\Renderer\Renderer as BaseRenderer;
+use Zapheus\Renderer\RendererInterface;
+
+/**
+ * Renderer
+ *
+ * @package Zapheus
+ * @author  Rougin Royce Gutib <rougingutib@gmail.com>
+ */
+class Renderer extends BaseRenderer implements RendererInterface
+{
+}
